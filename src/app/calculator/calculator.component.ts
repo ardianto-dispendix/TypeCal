fcd08ef2c0ddef194b8614136f364ce49d895dd2
ardianto nugroho <ardianto.nugroho@dispendix.com>
2026-02-01 10:32:28 +0700

    fix: compile error

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild  } from '@angular/core';
 import * as math from 'mathjs';
 
 interface CalculationResult {
